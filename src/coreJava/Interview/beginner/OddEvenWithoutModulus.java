e7b86b3563e4f661db39811722d9607dd990bae1
akshaybabbar12@gmail.com <akshaybabbar12@gmail.com>
2020-04-29 18:03:08 +0530

    Bitwise Solution Entered

--- src/coreJava/Interview/beginner/OddEvenWithoutModulus.java
+++ src/coreJava/Interview/beginner/OddEvenWithoutModulus.java
@@ -1,6 +1,19 @@
 package coreJava.Interview.beginner;
 
+import java.util.Scanner;
+
 /*
  * Author: Akshay Babbar*/
 public class OddEvenWithoutModulus {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter the number which has to be checked. ");
+        int number = scanner.nextInt();
+
+        String output = ((number & 1) == 0) ? "even" : "odd";
+
+        System.out.println("The number entered is " + output);
+
+    }
 }
