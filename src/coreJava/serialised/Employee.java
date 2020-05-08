366bbffcf9c584ca5b1adfbe7d1c4554ab20ea68
akshaybabbar12@gmail.com <akshaybabbar12@gmail.com>
2020-05-08 17:08:34 +0530

    Serialisation Basics Added

---
+++ src/coreJava/serialised/Employee.java
@@ -0,0 +1,26 @@
+package coreJava.serialised;
+
+/*
+ *
+ * Author: Akshay Babbar
+ * Purpose: This is driver class.
+ * */
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+    private Integer id;
+    private String name;
+
+    public Employee(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id = " + this.id + " name = " + this.name + "]";
+
+    }
+}
