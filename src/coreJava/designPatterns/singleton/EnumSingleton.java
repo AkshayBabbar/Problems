fa59cb92841c9a3302cfbdeff130ec473cf79d41
akshaybabbar12@gmail.com <akshaybabbar12@gmail.com>
2020-05-15 01:59:11 +0530

    Singleton

---
+++ src/coreJava/designPatterns/singleton/EnumSingleton.java
@@ -0,0 +1,17 @@
+package coreJava.designPatterns.singleton;
+
+
+/*
+*       Author:     Akshay Babbar
+*       Purpose:    Enum based singleton implementation. Effective Java 2nd Edition (Joshua Bloch) p. 18
+*                   This implementation is thread safe, however adding any other method and its thread safety
+*                   is developers responsibility.
+/*/
+public enum EnumSingleton {
+
+    INSTANCE;
+
+    public static void doSomething() {
+
+    }
+}
