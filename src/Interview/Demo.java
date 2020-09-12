21dab4bc572c1ca0c71b5ca9e661d519aecf191b
Akshay Babbar <akshaybabbar12@gmail.com>
2020-09-12 23:44:01 +0530

    Prototype Design Pattern

--- src/Interview/Demo.java
+++ src/Interview/Demo.java
@@ -1,8 +1,5 @@
 package Interview;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by Akshay Babbar on 21-08-2017.
  */
@@ -11,20 +8,20 @@ public class Demo {
     //    static void check(){
 //        variable = 16;
 //    }
-    private static int x = 10;
-    private static int y = 10;
-    public int b = 9;
-    protected int a = 10;
-
-
-    protected Demo(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    private Demo() {
-        this(5);
-    }
+//    private static int x = 10;
+//    private static int y = 10;
+//    public int b = 9;
+//    protected int a = 10;
+//
+//
+//    protected Demo(int a, int b) {
+//        this.a = a;
+//        this.b = b;
+//    }
+//
+//    private Demo() {
+//        this(5);
+//    }
 
 
 //    public static void main(String[] args) {
@@ -40,29 +37,42 @@ public class Demo {
 //        System.out.println(s.indexOf(s));
 //    }
 
-    private Demo(int x) {
-        this(x, y);
+//    private Demo(int x) {
+//        this(x, y);
+//    }
+
+    static Integer getAsci(String str) {
+        return str.length();
     }
 
     public static void main(String[] args) {
-        Demo t = new Demo();
 
-        String s1 = "Java";
-        String s2 = "Java";
-        String s3 = new String("Java");
+        String[] cmd = new String[1];
+        cmd[0] = "file.java";
 
-        System.out.println(s1 == s2);
-        System.out.println(s1 == s3);
-        List list = new ArrayList();
+//        Process process = Runtime.getRuntime().exec(cmd);
+
+        System.out.println("Test");
+
     }
-
-    protected void add() {
-        System.out.println(a + b);
-    }
-
-    private double Hackr() throws Exception {
-        return 0.7;
-    }
+//        Demo t = new Demo();
+//
+//        String s1 = "Java";
+//        String s2 = "Java";
+//        String s3 = new String("Java");
+//
+//        System.out.println(s1 == s2);
+//        System.out.println(s1 == s3);
+//        List list = new ArrayList();
+//    }
+//
+//    protected void add() {
+//        System.out.println(a + b);
+//    }
+//
+//    private double Hackr() throws Exception {
+//        return 0.7;
+//    }
 }
 
 
---
+++ src/coreJava/designPatterns/prototype/Employees.java
@@ -0,0 +1,38 @@
+package coreJava.designPatterns.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employees implements Cloneable {
+    private List<String> empList;
+
+    public Employees(List<String> empList) {
+        this.empList = empList;
+    }
+
+    public Employees() {
+        empList = new ArrayList<String>();
+
+    }
+
+    public void loadData() {
+        empList.add("Pankaj");
+        empList.add("Raj");
+        empList.add("David");
+        empList.add("Lisa");
+    }
+
+    public List<String> getEmpList() {
+        return empList;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        List<String> temp = new ArrayList<String>();
+        for (String s : this.getEmpList()) {
+            temp.add(s);
+        }
+        return new Employees(temp);
+
+    }
+}
