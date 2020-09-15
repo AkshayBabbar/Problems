6b9257d065ef2462cd3c65f91104afced3bd3635
Akshay Babbar <akshaybabbar12@gmail.com>
2020-09-16 03:21:03 +0530

    Creation of ThreadPool

--- src/coreJava/concurrency/ThreadExample2.java
+++ src/coreJava/concurrency/ThreadExample2.java
@@ -9,13 +9,17 @@ public class ThreadExample2 {
 
     public static void main(String[] args) {
         MyThread myThread = new MyThread();
+        myThread.run();
+        System.out.println("This is after run() Method");
         myThread.start();
+        System.out.println("This is after start() Method");
+
     }
 
     public static class MyThread extends Thread {
         public void run() {
-            System.out.println("MyThread is running");
-            System.out.println("MyThread is finished");
+            System.out.println("MyThread is running " + Thread.currentThread().getName());
+            System.out.println("MyThread is finished " + Thread.currentThread().getName());
         }
     }
 }
---
+++ src/coreJava/concurrency/ThreadPool.java
@@ -0,0 +1,42 @@
+package coreJava.concurrency;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class ThreadPool {
+    private BlockingQueue<Runnable> taskQueue;
+
+    private boolean shutDownInitiation = false;
+
+    public ThreadPool(int nThreads) {
+
+        taskQueue = new LinkedBlockingDeque<>(nThreads);
+
+        for (int i = 1; i <= nThreads; i++) {
+            ThreadPoolThreads threadPoolThreads = new ThreadPoolThreads(taskQueue, this);
+            threadPoolThreads.setName("Thread -" + i);
+            System.out.println("Thread- " + i + " created in ThreadPool");
+            threadPoolThreads.start();
+
+        }
+    }
+
+    public synchronized void execute(Runnable task) throws Exception {
+        if (this.shutDownInitiation) {
+            throw new Exception("THreadPool has been shutDown, no further update");
+        }
+        System.out.println("Task has been added");
+        this.taskQueue.put(task);
+    }
+
+    public boolean isShutDownInitiation() {
+        return shutDownInitiation;
+    }
+
+    public synchronized void shutDown() {
+        this.shutDownInitiation = true;
+        System.out.println("THREADPOOL SHUTDOWN INITIATED !!!!!!!");
+    }
+
+
+}
