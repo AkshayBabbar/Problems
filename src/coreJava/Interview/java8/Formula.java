b277b48a80a6bf047616e464828c64499886ddf2
Akshay Babbar <akshaybabbar12@gmail.com>
2020-09-20 23:05:21 +0530

    JAVA8

--- src/coreJava/Interview/java8/Formula.java
+++ src/coreJava/Interview/java8/Formula.java
@@ -4,11 +4,11 @@ public interface Formula {
 
     double calculate(int a);
 
-    default double sqrt(int a) {
+    static double sqrt(int a) {
         return Math.sqrt(a);
     }
 
-    default long rect(int a, int b) {
+    static long rect(int a, int b) {
         return a * b;
     }
 
