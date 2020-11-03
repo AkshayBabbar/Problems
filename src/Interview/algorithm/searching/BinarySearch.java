6dfb14de16853a6e7f026d5a57e858ad986927fe
Akshay Babbar <akshaybabbar12@gmail.com>
2020-11-04 01:09:04 +0530

    Adding BinarySearch

--- src/Interview/algorithm/searching/BinarySearch.java
+++ src/Interview/algorithm/searching/BinarySearch.java
@@ -1,8 +1,29 @@
 package Interview.algorithm.searching;
 
+/**
+ * @author Akshay Babbar
+ * @purpose Generic Binary Search to be inserted.
+ */
 public class BinarySearch {
 
-//    public static int binarySearch(int[] arr, int x){
-//        return                  0-
-//    }
+    /**
+     * @params: integer array is passed.
+     * @Params: integer number passed to be searched.
+     */
+    public static int binarySearch(int[] arr, int number) {
+        int right = arr.length - 1;
+        int leftbound = 0;
+        while (leftbound <= right) {
+            int mid = leftbound + (right - 1) / 2;
+            if (arr[mid] == number) {
+                return number;
+            }
+            if (arr[mid] < number) {
+                leftbound = mid + 1;
+            } else
+                right = mid - 1;
+        }
+        return -1;
+    }
 }
+
--- src/Interview/algorithm/searching/Driver.java
+++ src/Interview/algorithm/searching/Driver.java
@@ -1,4 +1,17 @@
 package Interview.algorithm.searching;
 
 public class Driver {
+    public static void main(String[] args) {
+
+        BinarySearch ob = new BinarySearch();
+        int arr[] = {2, 3, 4, 10, 40};
+        int n = arr.length;
+        int x = 10;
+        int result = ob.binarySearch(arr, x);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at "
+                    + "index " + result);
+    }
 }
