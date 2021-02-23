10b74171b3f6a0dc0ce69bba82845153ab830d20
Akshay Babbar <akshaybabbar12@gmail.com>
2021-02-23 23:41:04 +0530

    Adding Reversal.java

--- src/Interview/linearDataStructure/Array/rotations/Reversal.java
+++ src/Interview/linearDataStructure/Array/rotations/Reversal.java
@@ -1,4 +1,40 @@
 package Interview.linearDataStructure.Array.rotations;
 
 public class Reversal {
+    public static void leftRotate(int[] arr, int d) {
+        if (d == 0)
+            return;
+
+        int n = arr.length;
+        d = d % n;
+        rotate(arr, 0, d - 1);
+        rotate(arr, d, n - 1);
+        rotate(arr, 0, n - 1);
+    }
+
+    static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+
+    /*Function to reverse arr[] from index start to end*/
+    public static void rotate(int[] arr, int start, int end) {
+        int temp;
+        while (start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7};
+        int n = arr.length;
+        int d = 2;
+
+        leftRotate(arr, d); // Rotate array by d
+        printArray(arr);
+    }
 }
