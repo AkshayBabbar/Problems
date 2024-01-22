a7cda44ba71bc76651870f2b102d022a4bca2c53
akshaybabbar <akshaybabbar12@gmail.com>
2024-01-23 00:58:05 +0530

    Iterator and Iterable init

---
+++ src/scaler/core/January23/Node.java
@@ -0,0 +1,13 @@
+package src.scaler.core.January23;
+
+public class Node {
+    public Node next;
+    public int data;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+
+}
