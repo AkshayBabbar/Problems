a4a2293722a4c4d141b9b830e2832cd7e09b5493
akshaybabbar <akshaybabbar12@gmail.com>
2024-02-03 17:39:03 +0530

    Demo java 8

--- src/scaler/core/feb3/Client.java
+++ src/scaler/core/feb3/Client.java
@@ -7,6 +7,11 @@ public class Client {
         TableGenerator tableGenerator = new TableGenerator(4);
         ScalerThread thread = new ScalerThread(tableGenerator);
 
+        ScalerThread test = new ScalerThread(()-> {
+            System.out.println("Printing via lambda " + Thread.currentThread().getName());
+        });
+
+        test.start();
         thread.start();
     }
 }
