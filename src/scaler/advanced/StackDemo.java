6ffa2ff6c41e1b7387fbcc8deb73bab5e940ebae
akshaybabbar <akshaybabbar12@gmail.com>
2024-03-07 00:38:43 +0530

    DSA Update

--- src/scaler/advanced/StackDemo.java
+++ src/scaler/advanced/StackDemo.java
@@ -1,30 +1,24 @@
 package src.scaler.advanced;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.Stack;
+import java.util.*;
 
 public class StackDemo {
 
-    public static ArrayList<Integer> nextGreater(ArrayList<Integer> A) {
-        ArrayList<Integer> output = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-        stack.add(A.get(0));
-        int i = 1;
-        while (i < A.size()) {
-            int element = A.get(i);
-            int top = stack.peek();
-            if (top > element) {
-                output.add(-1);
-            } else {
-                output.add(element);
-                stack.add(element);
-            }
-        }
-        return output;
-    }
 
+    /**
+     * Push the first element in the stack.
+     * Pick rest of the elements one by one and follow the following steps in loop
+     * Mark the current element as next.
+     * If stack is not empty, compare top element of stack with next.
+     * If next is greater than the top element, Pop element from stack. Next is the next greater element for the popped element.
+     * Keep popping from the stack while the popped element is smaller than next. Next becomes the next greater element for all such popped elements.
+     * Finally, push the next in the stack.
+     * After the loop in step 2 is over, pop all the elements from stack and print -1 as next element for them.
+     * Time Complexity:- O(N)
+     *
+     * @param A
+     * @return
+     */
     public static int braces(String A) {
         Deque<Character> stack = new ArrayDeque();
         int strLen = A.length();
@@ -101,10 +95,79 @@ public class StackDemo {
 //        return output;
 //    }
 
+    public static ArrayList<Integer> nextGreater(ArrayList<Integer> A) {
+        ArrayList<Integer> output = new ArrayList<>(A.size());
+        if (A.size() < 2) {
+            output.add(-1);
+            return output;
+        }
+        for (Integer i : A) {
+            output.add(0);
+        }
+        int n = A.size();
+//        Stack<Integer> stack = new Stack<>();
+//        stack.add(A.get(A.size() - 1));
+        output.set(n - 1, -1);
+
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (A.get(i) > A.get(i + 1)) {
+                if (A.get(i) > output.get(i + 1)) {
+                    output.set(i, -1);
+                }
+                if (A.get(i) < output.get(i + 1)) {
+                    output.set(i, output.get(i + 1));
+                }
+
+            }
+            if (A.get(i) < A.get(i + 1)) {
+                output.set(i, A.get(i + 1));
+            }
+        }
+//
+//            while (!stack.empty() && A.get(stack.peek()) < A.get(i)) {
+//                stack.pop();
+//            }
+//            if (stack.isEmpty()) {
+//                output.set(i, A.get(stack.peek());
+//                stack.push(i);
+//
+//            }
+//
+//            // Push current element's index onto the stack
+//            stack.push(i);
+//        }
+//        int i = A.size() - 1;
+//        while (i > 0 && !stack.isEmpty()) {
+//            int curr = A.get(i);
+//            int top = stack.peek();
+//            if (curr < top) {
+//                stack.pop();
+//                output.add(top);
+//                stack.push(curr);
+//                i--;
+//            } else {
+//                output.add(-1);
+//                stack.push(curr);
+//                i--;
+//            }
+//        }
+        return output;
+
+    }
+
     public static void main(String[] args) {
 //        ArrayList<Integer> TC = new ArrayList<>(Arrays.asList(34, 35, 27, 42, 5, 28, 39, 20, 28));
 //        nextGreater(TC);
-        System.out.println(bracesTwo("((a+b))"));
+        ArrayList<Integer> output = new ArrayList<>(Arrays.asList(34, 35, 27, 42, 5, 28, 39, 20, 28));
+//        35 42 42 -1 28 39 -1 28 -1
+
+
+        ArrayList<Integer> next = nextGreater(output);
+        for (Integer i : next) {
+            System.out.print(i + " ,");
+        }
+//        System.out.println(bracesTwo("((a+b))"));
 
     }
 }
