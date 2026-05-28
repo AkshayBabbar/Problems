46216182e98d75c82ba6ff427a58c418b21ac933
akshaybabbar <akshaybabbar12@gmail.com>
2026-05-29 01:25:27 +0530

    Builder Pattern

---
+++ src/corejava/designpatterns/creational/builder/EmailRequest.java
@@ -0,0 +1,59 @@
+package src.corejava.designpatterns.creational.builder;
+
+import java.lang.module.ModuleDescriptor.Builder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmailRequest {
+
+    private String to;
+    private String from;
+    private String body;
+    private List<String> cc;
+    private List<String> bcc;
+    private int retryCount;
+
+    public EmailRequest(Builder builder) {
+        this.to = builder.to;
+        this.from = builder.from;
+        this.body = builder.body;
+        this.cc = builder.cc;
+        this.bcc = builder.bcc;
+
+    }
+
+    public static class Builder {
+        private String to;
+        private String from;
+        private String body;
+        private List<String> cc = new ArrayList<>();
+        private List<String> bcc = new ArrayList<>();
+        private int retryCount = 0;
+
+        public Builder(String to, String from, String body) {
+            this.to = to;
+            this.from = from;
+            this.body = body;
+        }
+
+        public Builder cc(List<String> cc) {
+            this.cc = cc;
+            return this;
+        }
+
+        public Builder bcc(List<String> bcc) {
+            this.bcc = bcc;
+            return this;
+        }
+
+        public Builder retryCount(int retryCount) {
+            this.retryCount = retryCount;
+            return this;
+        }
+
+        public EmailRequest build() {
+            return new EmailRequest(this);
+        }
+    }
+
+}
